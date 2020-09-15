2de2001917e3c3ad9504dff3446214ae99ee1c24
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-09-15 17:49:49 +0900

    feat : CORS Header

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const corsOption = {
 };
 app.use(cors(corsOption));
 
+app.use((req, res, next) => { 
+    res.header("Access-Control-Allow-Origin", "*") 
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); 
+        return res.status(200).json({});
+    }
+    next();
+})
 
 const bodyParser = require('body-parser')
 
@@ -21,5 +33,5 @@ app.listen(process.env.PORT || 3000)
 app.use('/register', register)
 
 app.get('/', (req, res) =>{ 
-    res.send('Dorothy API')
+    res.send('Dorothy API : I HATE CORS')
 })
