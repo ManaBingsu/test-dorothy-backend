4f814dea46380ce8059fc42f6563a8cd423a78a7
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-09-15 19:02:48 +0900

    fix : CORS option

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
 const express = require('express')
 const app = express()
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.use(allowCrossDomain);
-app.use(app.router);
+const cors = require('cors');
+app.use(cors());
+app.options('*', cors());  // enable pre-flight
   
 const bodyParser = require('body-parser')
 
@@ -30,5 +16,5 @@ app.listen(process.env.PORT || 3000)
 app.use('/register', register)
 
 app.get('/', (req, res) =>{ 
-    res.send('Dorothy API : I HATE CORS')
+    res.send('Dorothy API : I REALLY HATE CORS')
 })
