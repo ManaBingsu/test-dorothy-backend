7cf60aa0a0bd8e893f33c0ab47d56f1e5643e6aa
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-09-15 10:17:49 +0900

    feat : Find info from DB

--- router/register/index.js
+++ router/register/index.js
@@ -5,28 +5,28 @@ var router = express.Router()
 var path = require('path')
 const { User } = require('../../models/User')
 const config = require('../../config/key')
- /*
+
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
-*/
+
 router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 
-    return res.status(200).json({
-        success: "API request test"
-    })
-
+    User.findOne( {"name":"HyunSeokTest001"}, function(err, users) {
+        if (err) return res.status(500).send("User 전체 조회 실패.");
+        res.status(200).send(users.name);
+    /*
     const user = new User(req.body)
 
     user.save((err, userInfo) => {
         if (err) return res.json({success: false, err})
         return res.status(200).json({
             success: true
-        })
+        })*/
     })
 })
 
