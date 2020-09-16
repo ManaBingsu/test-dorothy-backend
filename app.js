5ecd93d8ccc55d101f0c4b9b448616d8fff9745e
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-09-16 15:55:19 +0900

    feat : CORS test

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
 const express = require('express')
 const app = express()
-//const cors = require('cors');
-//app.use(cors());
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+const cors = require('cors');
+app.use(cors());
 
 const bodyParser = require('body-parser')
 
@@ -31,6 +13,13 @@ var register = require('./router/register/index')
 app.listen(process.env.PORT || 3000)
 
 app.use('/register', register)
+
+app.post('/test', (req, res) => {
+
+    return res.json({
+        success: true
+    })
+})
 
 app.get('/', (req, res) =>{ 
     res.send('Dorothy API : I REALLY REALLy HATE CORS')
